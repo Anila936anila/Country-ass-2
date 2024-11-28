8194630f7061bbbbd306626a2d582294db965b48
Anila Hunain <161087832+Anila936anila@users.noreply.github.com>
2024-11-29 04:09:53 +0500

    Add files via upload
    Country Assignment 2

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex">
+   <h1>Country List</h1>
+
+   <Link href="/country/pakistan" style={{margin: "10px", borderRadius:"10px", border: "2px solid black", padding:"10px" }}>
+   Pakistan</Link>
+   <Link href="/country/india" style={{margin: "10px", borderRadius:"10px", border: "2px solid black", padding:"10px" }}>
+   India</Link>
+   <Link href="/country/china" style={{margin: "10px", borderRadius:"10px", border: "2px solid black", padding:"10px" }}>
+   China</Link>
+   <Link href="/country/turkey" style={{margin: "10px", borderRadius:"10px", border: "2px solid black", padding:"10px" }}>
+   Turkey</Link>
+   <Link href="/country/iran" style={{margin: "10px", borderRadius:"10px", border: "2px solid black", padding:"10px" }}>
+   Iran</Link>
+   <Link href="/country/japan" style={{margin: "10px", borderRadius:"10px", border: "2px solid black", padding:"10px" }}>
+   Japan</Link>
+    </div>
+  );
+}
